e375be4d7729e5d84ba4cfc7ad37422260e99503
mbozugrace <mbozugrace@gmail.com>
2018-06-06 15:58:33 -0400

    Fixing Syntax Errors

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -24,7 +24,7 @@ class ProposalsController < ApplicationController
 		@proposal = @investor.proposals.create(proposal_params)
 		if @proposal.save
 			#redirect to the proposal show page
-			SystemLog.new( system_event: "#{current_user.lastname, current_user.email} created a proposal #{@proposal.title}", event_time: Time.now).save
+			SystemLog.new( system_event: "#{current_user.lastname}, #{current_user.email} created a proposal #{@proposal.title}", event_time: Time.now).save
 			flash[:success] = "Your proposal has been successfully submitted. View the status of all Proposals by clicking the Proposal Tab."
 			redirect_to current_user
 		else
