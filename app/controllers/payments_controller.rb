a603f1f05a4c4a3ffd2ff73d00fadb5d79d5ba4d
mbozugrace <mbozugrace@gmail.com>
2018-06-23 11:56:24 -0400

    Added Scheduler

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -4,7 +4,7 @@ class PaymentsController < ApplicationController
 
   def check_out
   	#Stripe.api_key = "sk_test_ErANBi8jGcyUrgaJnNQkzWYR"
-  	Stripe.api_key = ENV['STRIPE_LIVE_SECRET_KEY']
+  	Stripe.api_key =  :secret_key_base
   	# Token is created using Checkout or Elements!
 	# Get the payment token ID submitted by the form:
 	token = params[:stripeToken]
